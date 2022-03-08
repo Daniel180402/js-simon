92bcac7d7b29c97d460116c4f7c997349b22d4d1
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-03-08 16:18:10 +0100

    added user prompt function in js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 let randomNumbers = [];
+let userNumbers = [];
 let writeRandomNumbers = document.getElementById("random-numbers");
 
 for( i=0; i <  5; i++){
     randomNumbers.push(randomInteger(1, 100));
 }
 
-console.log(randomNumbers);
 writeRandomNumbers.innerHTML = "i numeri sono: " + randomNumbers;
+console.log(randomNumbers);
 
+setTimeout(userNumPrompt, 3000);
 
+//   per verificare la presenza di un elemento x in un array y => y.includes(x)
 
-
+for(i = 0; i < 5; i++){
+    if ( randomNumbers.include(userNum)){
+        console.log("numero " + i + " corretto")
+    }
+    else{
+        console.log("numero " + i + " errato")
+    }
+}
 
 function randomInteger(min, max){
     if (isNaN(parseInt(min)) || isNaN(parseInt(max))){
@@ -18,3 +28,12 @@ function randomInteger(min, max){
     }
     return (Math.floor(Math.random() * ((max + 1) - min) + min));
 }
+
+
+function userNumPrompt(){
+    for( i=0; i < 5; i++){
+        userNumbers.push(parseInt(prompt("inserisci il numero")));
+    }
+    return userNumbers;
+}
+
